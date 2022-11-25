ab2b62a617d7fcebf71603d4a4e69ed7a8f05932
Nikita <Shagalovprog@gmail.com>
2022-11-25 23:53:32 +0300

    Refactor, make JsonFillerWindow

--- app_main.py
+++ app_main.py
@@ -1,10 +1,21 @@
-from WindowHandler import *
+import tkinter
+import json
 
+from tkinter import *
+from tkinter import scrolledtext
+from tkinter.ttk import Combobox
+
+from JsonFillerWindow import *
 
 def main():
-    window = WindowHandler()
+    window = Tk()
+    json_filler_window = JsonFillerWindow(
+        window=window,
+        window_resolution="1000x1000",
+        input_json_boxes_resolution=(40, 20)
+    )
+    json_filler_window.start()
 
-    window.start()
 
 if __name__ == '__main__':
     main()
